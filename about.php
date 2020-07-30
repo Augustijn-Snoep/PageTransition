e830f11964a1c75eae77be72efb68c8458d2d345
Augustijn-Snoep <60051491+Augustijn-Snoep@users.noreply.github.com>
2020-07-30 16:22:33 +0200

    update site meta

--- about.php
+++ about.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="Al jouw (geld)zaken goed geregeld!">
+        <meta name="description" content="A single page transition">
         <title>Page Transition</title>
         <link rel="stylesheet" type="text/css" href="dist/css/style.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap-grid.min.css">
